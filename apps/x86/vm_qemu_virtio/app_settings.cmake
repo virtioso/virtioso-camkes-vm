6ea4453216af04324dd527c030057fba8d00cb12
Hannu Lyytinen <hannu.lyytinen@unikie.com>
2026-04-21 17:57:40 +0300

    Add x86 vm_qemu_virtio Q35 guest target

---
+++ apps/x86/vm_qemu_virtio/app_settings.cmake
@@ -0,0 +1,18 @@
+#
+# Copyright 2026, Technology Innovation Institute
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+cmake_minimum_required(VERSION 3.8.2)
+
+if(NOT "${KernelPlatform}" STREQUAL "pc99")
+    message(FATAL_ERROR "KernelPlatform: ${KernelPlatform} not supported. Supported: pc99")
+endif()
+
+set(VM_IMAGE_MACHINE "qemux86-64")
+set(CAmkESVMGuestDMAIommu ON CACHE BOOL "" FORCE)
+set(KernelSel4Arch x86_64 CACHE STRING "" FORCE)
+set(KernelX86_64VTX64BitGuests ON CACHE BOOL "" FORCE)
+set(KernelMaxNumNodes 1 CACHE STRING "" FORCE)
+set(LibSel4VMMUseHPET ON CACHE BOOL "" FORCE)

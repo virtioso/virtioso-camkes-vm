821f2bd63951fe3b0429c93276983f4118cce005
Hannu Lyytinen <hannu.lyytinen@unikie.com>
2026-04-21 17:54:43 +0300

    Remove obsolete hyp_ftrace support

--- src/irq_line.c
+++ src/irq_line.c
@@ -7,8 +7,6 @@
 #include <sel4vm/guest_irq_controller.h>
 
 #include <virtioso/irq_line.h>
-#include <virtioso/camkes/hyp_ftrace.h>
-
 static void irq_line_ack(vm_vcpu_t *vcpu, int irq, void *cookie)
 {
 }
@@ -31,16 +29,11 @@ int irq_line_init(irq_line_t *line, vm_vcpu_t *vcpu, unsigned int irq,
 
 int irq_line_change(irq_line_t *line, bool active)
 {
-    if (active) {
-        hyp_ftrace_irq_injected();
-    }
     return vm_set_irq_level(line->vcpu, line->irq, active);
 }
 
 int irq_line_pulse(irq_line_t *line)
 {
-    hyp_ftrace_irq_injected();
-
     int err = vm_set_irq_level(line->vcpu, line->irq, true);
     if (err) {
         return err;

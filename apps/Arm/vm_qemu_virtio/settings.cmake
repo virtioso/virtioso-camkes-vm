5276b425f0c77f8dd8f31cb25fbca7499cf028dc
Hannu Lyytinen <hannu.lyytinen@unikie.com>
2026-04-29 22:20:33 +0300

    orinagx: enable UARTI IRQ trace for vm_qemu_virtio

--- apps/Arm/vm_qemu_virtio/settings.cmake
+++ apps/Arm/vm_qemu_virtio/settings.cmake
@@ -43,5 +43,9 @@ elseif("${PLATFORM}" STREQUAL "rpi4")
 elseif("${PLATFORM}" STREQUAL "orinagx")
     # Set machine for image paths
     set(VM_IMAGE_MACHINE "vm-jetson-agx-orin")
+    set(VmGicV3IrqTrace ON CACHE BOOL "" FORCE)
+    set(VmGicV3IrqTraceIrq 178 CACHE STRING "" FORCE)
+    set(LibSel4VMGicV3IrqTrace ON CACHE BOOL "" FORCE)
+    set(LibSel4VMGicV3IrqTraceIrq 178 CACHE STRING "" FORCE)
 
 endif()
